adfda4272495c4c532638c7ebf22724bdf6449c3
Rui Cesista <rui.cesista@inria.fr>
2026-05-18 14:38:04 +0200

    Add mapping order for the gpu thread and block And fix some primitive and type problem

--- src/xtc/itf/schd/scheduler.py
+++ src/xtc/itf/schd/scheduler.py
@@ -333,7 +333,7 @@ class Scheduler(ABC):
         ...
 
     @abstractmethod
-    def gpu_thread(self, axes: list[str]) -> None:
+    def gpu_thread(self, axes: list[str], root: str = DEFAULT_ROOT) -> None:
         """
         Map an axis to a certain gpu thread either x, y or z.
         The mapped axis need to be in parallelization.
@@ -342,7 +342,7 @@ class Scheduler(ABC):
         ...
 
     @abstractmethod
-    def gpu_block(self, axes: list[str]) -> None:
+    def gpu_block(self, axes: list[str], root: str = DEFAULT_ROOT) -> None:
         """
         Map an axis to a certain gpu block either x, y or z.
         The mapped axis need to be in parallelization.
